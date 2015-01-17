837633e2b73fb31855530d00d0cd001a6a74d366
Arunjit Singh <opensrc@ajsd.in>
2015-01-17 17:55:39 +0530

    fix tup file

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,7 +1,13 @@
 -- Tuprules.lua
 
 function compile(source, output)
-  tup.definerule{ inputs = {source}, command = 'clang ' .. source .. ' -o ' .. output, outputs = {output} }
+  tup.definerule{
+    inputs = {source},
+    command = 'clang ' .. source .. ' -o ' .. output,
+    outputs = {output},
+  }
 end
 
-compile('brainfuck.c', 'build/brainfuck')
+-- Tupfile.lua
+
+compile('brainfuck.c', 'brainfuck')
